110d5ff21436661636452db33a5897530f36e53d
nirranjjan <nirranjjan@gmail.com>
2024-01-13 12:46:14 +0530

    3 number sum solved

--- arrays.h
+++ arrays.h
@@ -39,3 +39,30 @@ void twoNumberSumUsingTraversers(vector<int> array, int targetSum) {
     }  
   cout << "" << endl;
 }
+
+vector<vector<int>> threeNumberSum(vector<int> array, int targetSum) {
+  vector<vector<int>> ret;
+  int sz = array.size();
+  sort(array.begin(), array.begin()+sz);
+  for(int i=0; i < sz; i++) {
+    int l = i+1, r = sz-1;
+    int cur = i, sum = 0;
+    cout << "i = " << i << " l = " << l << " r = " << r << " targetSum = " << targetSum <<endl;
+    while(l < r) {
+      sum = array[i] + array[l] + array[r];
+      cout << "  sum = " << sum << endl;
+      if(sum == targetSum) {
+        ret.push_back({array[cur], array[l], array[r]});
+        cout << "  " <<array[cur] << "," << array[l] << "," << array[r] << endl;
+        l++;
+        r--;
+      } else if(sum < targetSum) {
+        l++;
+      } else {
+        r--;
+      }
+    }
+  }
+  return ret;
+}
+
--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ void arrays()
 	vector<int> array = {3,5,-4,8,11,1,-1,6};
 	twoNumberSumUsingHashMap(array, 10);
 	twoNumberSumUsingTraversers(array, 10);
+	threeNumberSum(array, 0);
 }
 
 int main()
